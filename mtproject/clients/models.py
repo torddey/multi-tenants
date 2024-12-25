82d26c78e8f79bbf08d48973f8f569ec8203247c
torddey <ea.torddey@gmail.com>
2024-12-25 20:48:47 +0000

    Fixed errors

--- mtproject/clients/models.py
+++ mtproject/clients/models.py
@@ -3,7 +3,7 @@ from app.models import Clients
 
 
 class Products(models.Model):
-    tenant = models.ForeignKey(Clients, on_delete=models.CASCADE, related_name="products")  # Linking Products to a Tenant
+    tenant = models.ForeignKey(Clients, on_delete=models.CASCADE, related_name="products", default=1)  # Linking Products to a Tenant
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
    
